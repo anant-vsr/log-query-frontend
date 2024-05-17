dec84f86dc3ca3a117992bdfcb13937aff2bb33e
Anant Vikram Singh Rathore <ananvikramsr.cs20@rvce.edu.in>
2024-05-17 22:11:06 +0530

    Final Code

---
+++ client/src/App.js
@@ -0,0 +1,345 @@
+
+
+// App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import LogForm from './components/LogForm';
+import LoginPage from './components/LoginPage'; // Import the LoginPage component
+import './App.css';
+import Register from './components/Register'; // Adjust the path as needed
+
+
+function App() {
+  const initialQueryParams = {
+    level: '',
+    message: '',
+    resourceId: '',
+    startDate: '',
+    endDate: '',
+    traceId: '',
+    spanId: '',
+    commit: '',
+    parentResourceId: '',
+  };
+
+  const [logs, setLogs] = useState([]);
+  const [queryParams, setQueryParams] = useState(initialQueryParams);
+  const [token, setToken] = useState('');
+  const [showLogin, setShowLogin] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+  // Check if there's a token in local storage
+  const storedToken = localStorage.getItem('authToken');
+  if (storedToken) {
+    setToken(storedToken);
+  }
+
+  // Fetch logs when the component mounts
+  if (token) {
+    fetchLogs();
+  }
+}, [token]);
+
+// Include queryParams and token as dependencies to refetch when they change
+
+  const axiosConfig = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+const apiUrl=process.env.REACT_APP_API_URL;
+
+const fetchLogs = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}/logs`, {
+      params: queryParams,
+      ...axiosConfig,
+    });
+
+    const fetchedLogs = response.data;
+    console.log(fetchedLogs);
+
+    if (fetchedLogs.length === 0) {
+      setError('No logs found matching the specified criteria.');
+    } else {
+      setError(null); // Clear previous error messages
+    }
+
+    setLogs(fetchedLogs);
+
+  } catch (error) {
+    console.error(error);
+    setError('Error fetching logs. Please try again.'); // Set error message
+  }
+};
+
+
+
+const fetchLogsByTraceId = async () => {
+  try {
+    const response = await axios.get('https://logging-query-interface-deployed.onrender.com/logsByTraceId', {
+      params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsBySpanId = async () => {
+  try {
+    const response = await axios.get('https://logging-query-interface-deployed.onrender.com/logsBySpanId', {
+      params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByCommit = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/logsByCommit', {
+      params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByParentResourceId = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/logsByParentResourceId', {
+      params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByLevel = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/logsByLevel', {
+      params: { level: queryParams.level },
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByMessage = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/logsByMessage', {
+       params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByResourceId = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/logsByResourceId', {
+      params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchLogsByTimestampRange = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}/logsByTimestampRange`, {
+     params: queryParams,
+      ...axiosConfig,
+    });
+    setLogs(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+  const handleLogin = (token) => {
+    localStorage.setItem('authToken', token);
+    setToken(token);
+  };
+  const handleLogout = () => {
+  // Remove the token from local storage
+  localStorage.removeItem('authToken');
+  setToken('');
+};
+
+
+  const handleLogInsert = () => {
+    // Trigger a log fetch when a new log is inserted
+    fetchLogs();
+  };
+
+  const handleSearch = () => {
+    // Trigger a log fetch when the search button is clicked
+     fetchLogs();
+    setQueryParams(initialQueryParams); // Reset search form fields
+  };
+  const handleSearchtimestamp = () => {
+    // Trigger a log fetch when the search button is clicked
+     fetchLogsByTimestampRange ();
+    setQueryParams(initialQueryParams); // Reset search form fields
+  };
+
+
+
+  return (
+    <div>
+      
+     {!token ? (
+  <>
+    {!showLogin ? (
+      <button onClick={() => setShowLogin(true)}>Switch to Login</button>
+    ) : (
+      <button onClick={() => setShowLogin(false)}>Switch to Register</button>
+    )}
+
+    {showLogin ? (
+      <LoginPage onLogin={handleLogin} />
+    ) : (
+      <Register onRegister={() => setShowLogin(true)} />
+    )}
+  </>
+) : (
+        <>
+          <h1>Log Query Interface</h1>
+
+          <p>Welcome! You are logged in.</p>
+          {/* {setError && <p className="error-message">{setError}</p>} */}
+          <button onClick={handleLogout}>Logout</button>
+
+          {/* Log insertion form */}
+          <LogForm token={token} onLogInsert={handleLogInsert} setError={setError}/>
+          {/* {setError && <p className="error-message">{setError}</p>} */}
+          {/* Input fields for search parameters */}
+          <label>
+            Level:
+            <input
+              type="text"
+              value={queryParams.level}
+              onChange={(e) => setQueryParams({ ...queryParams, level: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Message:
+            <input
+              type="text"
+              value={queryParams.message}
+              onChange={(e) => setQueryParams({ ...queryParams, message: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Resource ID:
+            <input
+              type="text"
+              value={queryParams.resourceId}
+              onChange={(e) => setQueryParams({ ...queryParams, resourceId: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Start Date:
+            <input
+              type="text"
+              placeholder="YYYY-MM-DDTHH:mm:ssZ"
+              value={queryParams.startDate}
+              onChange={(e) => setQueryParams({ ...queryParams, startDate: e.target.value })}
+            />
+          </label>
+
+          <label>
+            End Date:
+            <input
+              type="text"
+              placeholder="YYYY-MM-DDTHH:mm:ssZ"
+              value={queryParams.endDate}
+              onChange={(e) => setQueryParams({ ...queryParams, endDate: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Trace ID:
+            <input
+              type="text"
+              value={queryParams.traceId}
+              onChange={(e) => setQueryParams({ ...queryParams, traceId: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Span ID:
+            <input
+              type="text"
+              value={queryParams.spanId}
+              onChange={(e) => setQueryParams({ ...queryParams, spanId: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Commit:
+            <input
+              type="text"
+              value={queryParams.commit}
+              onChange={(e) => setQueryParams({ ...queryParams, commit: e.target.value })}
+            />
+          </label>
+
+          <label>
+            Parent Resource ID:
+            <input
+              type="text"
+              value={queryParams.parentResourceId}
+              onChange={(e) => setQueryParams({ ...queryParams, parentResourceId: e.target.value })}
+            />
+          </label>
+
+          {/* Buttons to trigger search */}
+          <button onClick={handleSearch}>Search</button>
+           <button onClick={handleSearchtimestamp}>Search by Timestamp Range</button>
+          {/* <button onClick={fetchLogsByMessage}>Search by Message</button>
+          <button onClick={fetchLogsByResourceId}>Search by Resource ID</button>
+          <button onClick={fetchLogsByTraceId}>Search by Trace ID</button>
+          <button onClick={fetchLogsBySpanId}>Search by Span ID</button>
+          <button onClick={fetchLogsByCommit}>Search by Commit</button>
+          <button onClick={fetchLogsByParentResourceId}>Search by Parent Resource ID</button> */}
+
+          {error && <p className="error-message">{error}</p>}
+
+          <ul>
+            {logs.map((log) => (
+              <li key={log._id}>
+                <strong>{log.level}:</strong> {log.message} -{' '}
+                {new Date(log.timestamp).toLocaleString()}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/LogForm.js
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LogForm = ({ onLogInsert, token, setError }) => {
+  const initialLogData = {
+    level: '',
+    message: '',
+    resourceId: '',
+    timestamp: '',
+    traceId: '',
+    spanId: '',
+    commit: '',
+    metadata: {
+      parentResourceId: '',
+    },
+  };
+
+  const [logData, setLogData] = useState(initialLogData);
+  
+const apiUrl=process.env.REACT_APP_API_URL;
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+
+  if (name.includes('.')) {
+    const [parent, child] = name.split('.');
+    console.log('Setting nested field:', parent, child, value);
+    setLogData((prevData) => ({
+      ...prevData,
+      [parent]: {
+        ...prevData[parent],
+        [child]: value,
+      },
+    }));
+  } else {
+    console.log('Setting regular field:', name, value);
+    setLogData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+};
+
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  console.log('Log data to be submitted:', logData);
+
+  // Check if any required fields are empty
+  const requiredFields = ['level', 'message', 'resourceId', 'timestamp', 'traceId', 'spanId', 'commit', 'metadata.parentResourceId'];
+const emptyFields = requiredFields.filter(field => {
+  if (field === 'metadata.parentResourceId') {
+    return !logData.metadata || !logData.metadata.parentResourceId;
+  } else {
+    return !logData[field];
+  }
+});
+
+console.log('Empty fields:', emptyFields);
+
+if (emptyFields.length > 0) {
+  setError(`Please fill in all required fields: ${emptyFields.join(', ')}`);
+  return;
+}
+
+  try {
+    // Ensure that metadata is set correctly
+    const dataToSend = {
+      ...logData,
+      metadata: {
+        parentResourceId: logData.metadata.parentResourceId,
+      },
+    };
+
+    console.log('Data to be sent:', dataToSend);
+
+    await axios.post(`${apiUrl}/ingest`, dataToSend, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log('Log inserted successfully');
+    onLogInsert();
+    setLogData(initialLogData); // Reset form data
+    setError(null); // Clear error message
+  } catch (error) {
+    console.error('Error inserting log:', error.response.data);
+    setError('Error inserting log. Please try again.'); // Set error message
+    setLogData(initialLogData); // Reset form data even in case of failure
+  }
+};
+
+
+
+
+
+  return (
+    <div>
+        {/* {setError && <p className="error-message">{setError}</p>} */}
+      <form onSubmit={handleSubmit}>
+      <label>
+        Level:
+        <input type="text" name="level" value={logData.level} onChange={handleChange} />
+      </label>
+
+      <label>
+        Message:
+        <input type="text" name="message" value={logData.message} onChange={handleChange} />
+      </label>
+
+      <label>
+        Resource ID:
+        <input type="text" name="resourceId" value={logData.resourceId} onChange={handleChange} />
+      </label>
+
+      <label>
+        Timestamp:
+        <input type="text" name="timestamp" value={logData.timestamp} onChange={handleChange} />
+      </label>
+
+      <label>
+        Trace ID:
+        <input type="text" name="traceId" value={logData.traceId} onChange={handleChange} />
+      </label>
+
+      <label>
+        Span ID:
+        <input type="text" name="spanId" value={logData.spanId} onChange={handleChange} />
+      </label>
+
+      <label>
+        Commit:
+        <input type="text" name="commit" value={logData.commit} onChange={handleChange} />
+      </label>
+
+      <label>
+        Parent Resource ID:
+        <input
+          type="text"
+          name="metadata.parentResourceId"
+          value={logData.metadata.parentResourceId}
+          onChange={handleChange}
+        />
+      </label>
+
+      {/* Add more input fields for other log properties */}
+
+      <button type="submit">Insert Log</button>
+     
+    </form>
+    </div>
+
+  );
+};
+
+export default LogForm;
